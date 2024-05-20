2d0bcfb9cec6f95e02255a57cc4dc8d7a7fcb587
Luca <luca.haar@icloud.com>
2024-05-20 18:12:19 +1000

    initialised mc 1.20 testing mod with blank state working

---
+++ src/main/java/io/github/codedbeats/testing_mod/TestingMod.java
@@ -0,0 +1,8 @@
+package io.github.codedbeats.testing_mod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TestingMod.MOD_ID)
+public class TestingMod {
+    public static final String MOD_ID = "testing_mod";
+}
