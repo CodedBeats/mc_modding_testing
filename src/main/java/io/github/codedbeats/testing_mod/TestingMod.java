2e28884ebdc8abc9a8a319fa4255fb15e397e94f
Luca <luca.haar@icloud.com>
2024-05-20 18:47:52 +1000

    food item, simple texture -- added a registry for items and added a simple food item (stats and effects) to it, added a very simple .png as a texture for the item so it can be tested cisibly in game

--- src/main/java/io/github/codedbeats/testing_mod/TestingMod.java
+++ src/main/java/io/github/codedbeats/testing_mod/TestingMod.java
@@ -1,8 +1,18 @@
 package io.github.codedbeats.testing_mod;
 
+import io.github.codedbeats.testing_mod.init.ItemInit;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(TestingMod.MOD_ID)
 public class TestingMod {
     public static final String MOD_ID = "testing_mod";
+
+    public TestingMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // add items register to bus
+        ItemInit.ITEMS.register(bus);
+    }
 }
---
+++ src/main/java/io/github/codedbeats/testing_mod/init/ItemInit.java
@@ -0,0 +1,28 @@
+package io.github.codedbeats.testing_mod.init;
+
+import io.github.codedbeats.testing_mod.TestingMod;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemInit {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestingMod.MOD_ID);
+
+    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
+            () -> new Item(new Item.Properties()
+                    .stacksTo(16)
+                    .food(new FoodProperties.Builder()
+                            .nutrition(5)
+                            .saturationMod(0.2f)
+                            .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 200, 2), 1f)
+                            .build())
+                    .rarity(Rarity.EPIC)
+            ));
+
+}
